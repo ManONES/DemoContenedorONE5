eaf6b6f324e44c4a81d6402714e7c376f3b6b7d4
ManONES <62667473+ManONES@users.noreply.github.com>
2020-03-30 17:52:18 -0600

    Add files via upload

---
+++ src/main/java/com/example/demo/DemoContenedorOneApplication.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@SpringBootApplication
+@Controller
+@EnableAutoConfiguration
+public class DemoContenedorOneApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello Many Word...";
+	}
+
+	@RequestMapping("/about")
+	@ResponseBody	
+	String about() {
+		return "About Hola mundo de Many......";
+	}	
+	
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(DemoContenedorOneApplication.class, args);
+	}
+	
+
+
+}
